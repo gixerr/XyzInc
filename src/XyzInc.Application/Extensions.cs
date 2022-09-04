0741903b2e00085344942a1a605b4f4296b62c50
gixerr <pwardyn213@gmail.com>
2022-09-04 15:57:12 +0200

    added IoC extension methods

---
+++ src/XyzInc.Application/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using XyzInc.Application.Dispatchers;
+using XyzInc.Application.Exceptions;
+using XyzInc.Application.Gateways;
+using XyzInc.Application.Services;
+using XyzInc.Application.Validators;
+
+namespace XyzInc.Application;
+
+public static class Extensions
+{
+    public static void AddApplication(this IServiceCollection services)
+        => services
+            .AddScoped<IOrderService, OrderService>()
+            .AddScoped<IPaymentGatewayDispatcher, PaymentGatewayDispatcher>()
+            .AddScoped<IOrderValidator, OrderValidator>()
+            .AddScoped<EuropePaymentGateway>()
+            .AddScoped<UnitedStatesPaymentGateway>();
+
+
+    public static void AddErrorHandling(this IServiceCollection services)
+        => services
+            .AddScoped<ErrorHandlerMiddleware>()
+            .AddSingleton<IExceptionToResponseMapper, ExceptionToResponseMapper>();
+
+    public static void UseErrorHandling(this IApplicationBuilder app)
+        => app
+            .UseMiddleware<ErrorHandlerMiddleware>();
+
+}
