8d4d99e6ef6184b2237f41a2028106e9b872a9cb
gixerr <pwardyn213@gmail.com>
2022-09-04 15:50:24 +0200

    added Order model

---
+++ src/XyzInc.Core/Domain/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace XyzInc.Core.Domain;
+
+public class Order
+{
+    public int OrderNumber { get; set; }
+    public Guid UserId { get; set; }
+    public decimal PayableAmount { get; set; }
+    public string Description { get; set; }
+}
