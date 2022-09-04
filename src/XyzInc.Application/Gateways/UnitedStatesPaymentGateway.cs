517d251029de4b22def0e6eaaf3812e33806cf1c
gixerr <pwardyn213@gmail.com>
2022-09-04 17:00:16 +0200

    added missing check in UnitedStatesPaymentGateway

--- src/XyzInc.Application/Gateways/UnitedStatesPaymentGateway.cs
+++ src/XyzInc.Application/Gateways/UnitedStatesPaymentGateway.cs
@@ -13,6 +13,10 @@ internal class UnitedStatesPaymentGateway : IPaymentGateway
     }
     public Receipt ProcessOrder(Order order)
     {
+        if (order is null)
+        {
+            throw new AppException("Gateway received null order. Order cannot be null.");
+        }
         _orderValidator.Validate(order);
 
         Receipt receipt = new()
