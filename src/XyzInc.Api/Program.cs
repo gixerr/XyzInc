29985feda320d62c512fd6541710fe0bc107600e
gixerr <pwardyn213@gmail.com>
2022-09-04 15:59:46 +0200

    Program.cs cleanup

--- src/XyzInc.Api/Program.cs
+++ src/XyzInc.Api/Program.cs
@@ -1,25 +1,20 @@
+using XyzInc.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddErrorHandling();
 
 var app = builder.Build();
+app.UseErrorHandling();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
