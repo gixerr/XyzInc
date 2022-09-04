5a20d154a78460cc9d7292b865c8c8209c5bb33c
gixerr <pwardyn213@gmail.com>
2022-09-04 15:55:09 +0200

    added controller

---
+++ src/XyzInc.Api/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XyzInc.Application.DTO;
+using XyzInc.Application.Services;
+
+namespace XyzInc.Api.Controllers;
+
+public class OrdersController : BaseController
+{
+    private readonly IOrderService _orderService;
+
+    public OrdersController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+    
+    [HttpPost]
+    public ActionResult ProcessOrder(OrderPostDto orderPostDto)
+    {
+        var receipt = _orderService.ProcessOrder(orderPostDto);
+
+        return Created("", receipt);
+    }
+}
